2b202a42cc4e3ace97dd26ac2cf0d531c32a48f2
dmk-hub <timothymanaya@gmail.com>
2026-02-17 21:37:54 +0700

    Hackerank

---
+++ DrawTheTriangle2.sql
@@ -0,0 +1,20 @@
+/*
+https://www.hackerrank.com/challenges/draw-the-triangle-2/problem?isFullScreen=true
+*/
+
+DELIMITER $$
+
+CREATE PROCEDURE WHILE_P()
+BEGIN
+    DECLARE i INT DEFAULT 1;
+    
+    WHILE i <= 20 DO
+        SELECT REPEAT('* ', i);
+        SET i = i + 1;
+    END WHILE;
+    
+END$$
+
+DELIMITER ;
+
+CALL WHILE_P();
