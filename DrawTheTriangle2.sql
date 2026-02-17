3f3d8fec2a095be718d100287c1f19a75c2450e3
dmk-hub <timothymanaya@gmail.com>
2026-02-17 21:41:12 +0700

    Hackerank

--- DrawTheTriangle2.sql
+++ DrawTheTriangle2.sql
@@ -7,14 +7,10 @@ DELIMITER $$
 CREATE PROCEDURE WHILE_P()
 BEGIN
     DECLARE i INT DEFAULT 1;
-    
     WHILE i <= 20 DO
         SELECT REPEAT('* ', i);
         SET i = i + 1;
     END WHILE;
-    
 END$$
-
 DELIMITER ;
-
 CALL WHILE_P();
